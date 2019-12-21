f3eef61190440928ee0bef3fc9ec055886b1e9ea
faisal3397 <faisal.alowaisi.97@gmail.com>
2019-12-21 16:38:47 +0300

    result page and installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import './App.css';
 import Home from './views/HomePage/Home.js'
+import Result from './views/ResultPage/Result.js'
 import 'typeface-roboto';
+import {
+  Route,
+  withRouter
+} from "react-router-dom";
 
-function App() {
+const renderHome = () => {  
+  return(
+    <Home />
+  )
+}
+
+const renderResult = () => {
+  return(
+    <Result />
+  )
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <div class="background-image"></div>
-      <div className="content">
-        <Home />
+      <div className="App">
+        <div className="background-image"></div>
+        <div className="content">
+          <Route exact path="/" render={renderHome} />
+          <Route exact path="/result" render={renderResult}/>
+        </div>
       </div>
-    </div>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/views/HomePage/Home.js
+++ src/views/HomePage/Home.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import './Home.css';
-import Typography from '@material-ui/core/Typography';
+import { useHistory } from "react-router-dom";
+import { Button, Typography, makeStyles } from '@material-ui/core';
 import logo from '../../asset/logo3.png'
 
-function home() {
+const useStyles = makeStyles({
+  btn: {
+    backgroundColor: '#03B3E4',
+    color: 'white',
+    width: '50%'
+  } 
+})
+
+const fetchSuggestion = (pos) => {
+  let latitude = pos.coords.latitude
+  let longitude = pos.coords.longitude
+  let uid = `${latitude},${longitude}`
+  console.log(uid);
+}
+
+const Home = () => {
+  const classes = useStyles()
+  let history = useHistory()
+
+  const suggest = () => {
+    navigator.geolocation.getCurrentPosition(fetchSuggestion); // get the latitude and longitude then pass it to the function fetchSuggestion()
+    history.push('/result')
+  }
+
   return (
       <div className="home">
         <img id="logo" src={logo} alt="logo"/>
         <Typography className="title" variant="h3" gutterBottom>
           Where Shall We Eat?
         </Typography>
+        <Button variant="contained" className={classes.btn} onClick={suggest}>
+          Suggest
+        </Button>
       </div>
   );
 }
 
-export default home
+export default Home
